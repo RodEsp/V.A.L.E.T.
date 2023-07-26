fe77cd60b7a679e2f495008697a5ce0731a6d9bd
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-07-26 17:50:50 -0400

    feat: get the sender mac address for any device connecting to a network by using ARP packets

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const Cap = require('cap').Cap;
 const decoders = require('cap').decoders;
 const PROTOCOL = decoders.PROTOCOL;
 
-console.dir(JSON.stringify(Cap.deviceList(), null, 2));
+console.log(JSON.stringify(Cap.deviceList(), null, 2));
 
 const cap = new Cap();
-const device = Cap.findDevice('10.100.0.1');
+// The device needs to be changes to the appropriate network device for the machine this is being run on, you can also use the IP address assigned to the machine with Cap.findDevice();
+const device = "wlan0"; // Cap.findDevice('10.100.2.85');
 
-const pcap_filter = '';
+console.log(`Device = ${device}`);
+
+const pcap_filter = 'arp'; // We are only interested in ARP packets - https://en.wikipedia.org/wiki/Address_Resolution_Protocol
 const bufSize = 10 * 1024 * 1024;
 const buffer = Buffer.alloc(65535);
 
@@ -15,30 +18,28 @@ const linkType = cap.open(device, pcap_filter, bufSize, buffer);
 
 cap.setMinBytes && cap.setMinBytes(0);
 
+const hexToIP = (hexString) => {
+  let bytes = hexString.match(/.{2}/g);
+
+  return bytes.reduce((acc, hexByte) => `${acc}.${parseInt(hexByte, 16)}`, '').substring(1);
+};
+
 cap.on('packet', (nbytes, trunc) => {
-  const destinationMacAddress = buffer.toString('hex', 0, 6).match(/.{2}/g).join(':');
+  // The first 6 bytes of a packet are the destination MAC Address
+  // The second 6 bytes of a packet are the source MAC Address
+  // See: https://en.wikipedia.org/wiki/Ethernet_frame for more information on a packet's makeup
   const sourceMacAddress = buffer.toString('hex', 6, 12).match(/.{2}/g).join(':');
 
-  const printPacketInfo = () => {
-    console.log(`  packet: length ${nbytes} bytes, truncated? ${trunc}`);
-    console.log(`  ${linkType}`);
-    console.log(`  Destination MAC Address: ${destinationMacAddress}
-Source MAC Address: ${sourceMacAddress}`);
-  };
+  if (linkType === 'ETHERNET') {
+    let packet = decoders.Ethernet(buffer);
 
-  switch (sourceMacAddress) {
-    case '8c:85:90:9a:01:7d':
-      printPacketInfo();
-      break;
-    case 'b8:27:eb:a0:fb:bb':
-      console.log('WLAN0 Device');
-      printPacketInfo();
-      break;
-    case 'c8:2a:dd:b0:7d:9d':
-      console.log('MY PHONE!!!');
-      printPacketInfo();
-      break;
-    default:
-      break;
+    console.log(`New packet:
+  ${nbytes} bytes, truncated? ${trunc}`);
+
+    packet = decoders.ARP(buffer, packet.offset);
+    console.log(`  Sender MAC Address: ${packet.info.sendermac}${packet.info.sendermac === 'c8:2a:dd:b0:7d:9d' ? ' - MY PHONE!!!' : ''}`);
+    console.log(`  Sender IP Address: ${packet.info.senderip}
+  Target IP Address: ${packet.info.targetip}
+  IPv4 Protocol: ${PROTOCOL.ETHERNET[packet.info.protocol]}`);
   }
 });
