d6461dceaa332c8288f619b14f52a1727ed14286
rodesp <1084688+RodEsp@users.noreply.github.com>
2023-08-28 10:48:40 -0400

    feat: add script to get number of vists created or updated by V.A.L.E.T.

---
+++ getVisits.js
@@ -0,0 +1,46 @@
+import fs from 'node:fs';
+
+const pat = fs.readFileSync('./pat').toString().trim();
+
+async function fetchDataUntilDate (targetDate) {
+	let today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+	let url = `https://www.recurse.com/api/v1/hub_visits?start_date=${targetDate}&end_date=${today}&page=1`;
+	let valetVisits = [];
+	let page = 1;
+
+	while (true) {
+		let response = await fetch(url, {
+			headers: {
+				'Authorization': `Bearer ${pat}`
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+
+		let visits = await response.json();
+
+		if (visits.length > 0) {
+			for (let visit of visits) {
+				if (visit.created_by_app === 'V.A.L.E.T.' || visit.updated_by_app === 'V.A.L.E.T.') {
+					valetVisits.push(visit);
+				}
+			}
+
+			// Set the next URL for pagination
+			page += 1;
+			url = `https://www.recurse.com/api/v1/hub_visits?start_date=${targetDate}&end_date=${today}&page=${page}`;
+		} else {
+			break;
+		}
+	}
+
+	return valetVisits;
+}
+
+// Example of usage:
+const since = '2023-08-23';
+fetchDataUntilDate(since)
+	.then(data => console.log(`Total V.A.L.E.T. visits since ${new Date(since).toDateString()}: ${data.length}`))
+	.catch(error => console.error("Error fetching data: ", error));
