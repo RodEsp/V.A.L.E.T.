660c67a99021cfde01e615225e3bedc0283c1f95
rodesp <1084688+RodEsp@users.noreply.github.com>
2024-02-19 15:36:24 -0500

    feat: add session to page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,21 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import Image from "next/image";
+
+import { Avatar, Button, Spacer } from '@nextui-org/react';
+
 import Header from './components/header';
-import { Button, Spacer } from '@nextui-org/react';
 
 export default function VALET() {
+  const { data: session } = useSession();
 
   return (
     <>
       <Header />
       <Spacer />
+      <Avatar src={session?.user?.image as string} size="lg" alt={`A picture of ${session?.user?.name}` as string}/>
+      <h3>Welcome {session?.user?.name}!</h3>
       <Button>
         <a href="/api/auth/signout">Logout</a>
       </Button>
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ['assets.recurse.com']
+	}
+};
 
 module.exports = nextConfig;
