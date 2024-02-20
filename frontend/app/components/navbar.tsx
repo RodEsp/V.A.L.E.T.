db3b70c5541063fc1353a0432cd6f7207a7575dd
rodesp <1084688+RodEsp@users.noreply.github.com>
2024-02-19 23:47:30 -0500

    feat: add mongodb connection

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,20 +1,21 @@
 'use client';
 
-import { Avatar, Button, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { Avatar, Button, CircularProgress, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
 
 export default function VNavbar(props: {name: string, url: string}) {
 	return (
 	<>
 		<Navbar isBlurred={false}>
 			<NavbarContent>V.A.L.E.T.</NavbarContent>
-			<NavbarContent>Welcome {props.name}!</NavbarContent>
+			<NavbarContent>Welcome {props.name ? `${props.name}!`: ''}</NavbarContent>
 			<NavbarItem>
 				<Button>
 					<a href="/api/auth/signout">Logout</a>
 				</Button>
 			</NavbarItem>
 			<NavbarItem>
-				<Avatar src={props.url} size="lg" alt={`A picture of ${props.name}` as string}/>
+				{props.url ? <Avatar src={props.url} size="lg" name={props.name} alt={`A picture of ${props.name}` as string}/>
+					: <CircularProgress aria-label="Loading..."/>}
 			</NavbarItem>
 		</Navbar>
 	</>
