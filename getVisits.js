7c0e8701c3bb72943aa6b305cf661ad5435dd1d4
RodEsp <1084688+RodEsp@users.noreply.github.com>
2025-07-22 21:33:58 -0400

    feat(getVisits.js): improve script output a bit

--- getVisits.js
+++ getVisits.js
@@ -1,29 +1,31 @@
-import fs from 'node:fs';
+const RC_PAT = process.env.RC_PAT;
 
-const pat = fs.readFileSync('./pat').toString().trim();
-
-async function fetchDataUntilDate (targetDate) {
-	let today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+async function fetchDataUntilDate(targetDate) {
+	const today = new Date().toISOString().split("T")[0]; // Get today's date in YYYY-MM-DD format
 	let url = `https://www.recurse.com/api/v1/hub_visits?start_date=${targetDate}&end_date=${today}&page=1`;
-	let valetVisits = [];
+	const valetVisits = [];
 	let page = 1;
 
 	while (true) {
-		let response = await fetch(url, {
+		process.stdout.write(`\rFetching page ${page}...`);
+		const response = await fetch(url, {
 			headers: {
-				'Authorization': `Bearer ${pat}`
-			}
+				Authorization: `Bearer ${RC_PAT}`,
+			},
 		});
 
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
 		}
 
-		let visits = await response.json();
+		const visits = await response.json();
 
 		if (visits.length > 0) {
-			for (let visit of visits) {
-				if (visit.created_by_app === 'V.A.L.E.T.' || visit.updated_by_app === 'V.A.L.E.T.') {
+			for (const visit of visits) {
+				if (
+					visit.created_by_app === "V.A.L.E.T." ||
+					visit.updated_by_app === "V.A.L.E.T."
+				) {
 					valetVisits.push(visit);
 				}
 			}
@@ -35,19 +37,29 @@ async function fetchDataUntilDate (targetDate) {
 			break;
 		}
 	}
+	process.stdout.write(`\r\x1b[K`); // Clear the entire line
+	process.stdout.write(`Fetched ${page} pages of Hub Visits\n`);
 
 	return valetVisits;
 }
 
-
-let date = Date.parse(process.argv[2]);
-let since = '2023-08-03';
+const date = Date.parse(process.argv[2]);
+let since = "2023-08-03";
 
 if (date) {
-  since = new Intl.DateTimeFormat('en-US', {year:'numeric', month:'2-digit', day:'2-digit', timeZone:'UTC'}).format(date);
-  console.log(since);
+	since = new Intl.DateTimeFormat("en-US", {
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+		timeZone: "UTC",
+	}).format(date);
+	console.log(`Getting visits since ${since}`);
 }
 
 fetchDataUntilDate(since)
-	.then(data => console.log(`Total V.A.L.E.T. visits since ${new Date(since).toDateString()}: ${data.length}`))
-	.catch(error => console.error("Error fetching data: ", error));
+	.then((data) =>
+		console.log(
+			`\nTotal V.A.L.E.T. visits since ${new Date(since).toDateString()}: ${data.length}`,
+		),
+	)
+	.catch((error) => console.error("Error fetching data: ", error));
